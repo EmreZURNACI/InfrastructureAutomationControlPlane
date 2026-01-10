ae66832a70b1e8d75a8204da1fd6593868cb4b5a
OsmanEmreZURNACI <emrezurnaci38@gmail.com>
2026-01-11 00:36:52 +0300

    feat(server): add route handler

---
+++ Proxy/server/handler.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"github.com/EmreZURNACI/InfrastructureAutomationControlPlaneProxy/infra/postgres"
+	"github.com/gofiber/fiber/v2"
+)
+
+func NewRoutesHandler(app *fiber.App, dep Dependencies) *routesHandler {
+	return &routesHandler{
+		App:          app,
+		Dependencies: dep,
+	}
+}
+
+type routesHandler struct {
+	Dependencies Dependencies
+	App          *fiber.App
+}
+type Dependencies struct {
+	DB *postgres.DB
+}
