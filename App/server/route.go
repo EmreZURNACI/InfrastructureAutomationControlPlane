14889152840b9bb17bc42d162f25dd7ee7a51fdd
OsmanEmreZURNACI <emrezurnaci38@gmail.com>
2026-01-11 22:53:09 +0300

    feat(server): add server initialization, middlewares, tracing and routes

---
+++ App/server/route.go
@@ -0,0 +1,52 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/EmreZURNACI/InfrastructureAutomationControlPlane/pkg/config"
+	"github.com/EmreZURNACI/InfrastructureAutomationControlPlane/pkg/log"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/gofiber/contrib/otelfiber"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
+)
+
+func Start(dep Dependencies) {
+
+	app := fiber.New(fiber.Config{
+		AppName: "MyService",
+
+		ServerHeader:          "EmreZURNACI - InfrastructureAutomationControlPlane",
+		DisableStartupMessage: true,
+
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	})
+
+	app.Use(race_limiter)
+	app.Use(cors_handler)
+	app.Use(otelfiber.Middleware())
+
+	app.Add(http.MethodGet, "/metrics", adaptor.HTTPHandler(promhttp.Handler()))
+
+	app.Use(InternalOnly)
+
+	routes := NewRoutesHandler(app, dep)
+	routes.StartHello()
+	routes.StartLiveness()
+	routes.StartAdmin()
+	routes.StartInstance()
+	routes.StartVolumes()
+
+	log.Logger.Info(fmt.Sprintf("HTTP server started in %s:%d", config.AppConfig.ServerConfig.Host, config.AppConfig.ServerConfig.Port))
+	if err := app.Listen(fmt.Sprintf("%s:%d", config.AppConfig.ServerConfig.Host, config.AppConfig.ServerConfig.Port)); err != nil {
+		log.Logger.Error("HTTP server failed")
+	}
+
+	gracefulShutdown(app)
+
+}
