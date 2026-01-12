f4399521c1424828c4ea7e13b3c5b1412797de49
OsmanEmreZURNACI <emrezurnaci38@gmail.com>
2026-01-13 01:37:00 +0300

    feat(server): add ldap connections and signup endpoint

--- Proxy/server/auth.go
+++ Proxy/server/auth.go
@@ -10,11 +10,14 @@ import (
 func (h *routesHandler) StartAuth() {
 	route := h.App.Group("/api").Group("/v1").Group("/auth")
 
-	signInService := authsrv.NewSignInService(h.Dependencies.DB)
+	signInService := authsrv.NewSignInService(h.Dependencies.DB, h.Dependencies.Ldap)
+	SignUpService := authsrv.NewSignUpService(h.Dependencies.Ldap)
 	authService := auth.AuthService{
 		SignInService: signInService,
+		SignUpService: SignUpService,
 	}
 
 	route.Add(http.MethodPost, "/signin", authService.SignIn)
+	route.Add(http.MethodPost, "/signup", authService.SignUp)
 
 }
