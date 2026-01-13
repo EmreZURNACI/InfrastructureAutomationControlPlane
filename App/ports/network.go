202bd175ac042c3c69c73daebfd2eca29d5156e6
OsmanEmreZURNACI <emrezurnaci38@gmail.com>
2026-01-13 23:00:48 +0300

    feat(ports): add network interface definition

---
+++ App/ports/network.go
@@ -0,0 +1,13 @@
+package ports
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+)
+
+type NetworkClient interface {
+	DescribeVpcs(ctx context.Context, params *ec2.DescribeVpcsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeVpcsOutput, error)
+	DescribeSubnets(ctx context.Context, params *ec2.DescribeSubnetsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeSubnetsOutput, error)
+	DescribeSecurityGroups(ctx context.Context, params *ec2.DescribeSecurityGroupsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeSecurityGroupsOutput, error)
+}
