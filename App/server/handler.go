2b7d8e3b95ad5fcb8742f5ac2016f8feb21d73b7
OsmanEmreZURNACI <emrezurnaci38@gmail.com>
2026-01-13 23:08:44 +0300

    feat(server)

---
+++ App/server/handler.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	"github.com/EmreZURNACI/InfrastructureAutomationControlPlane/adaptor/client"
+	"github.com/EmreZURNACI/InfrastructureAutomationControlPlane/infra/postgres"
+	"github.com/gofiber/fiber/v2"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func NewRoutesHandler(app *fiber.App, dep Dependencies) *routesHandler {
+	return &routesHandler{
+		App:          app,
+		Dependencies: dep,
+	}
+}
+
+type routesHandler struct {
+	Dependencies Dependencies
+	App          *fiber.App
+}
+type Dependencies struct {
+	EC2Client *client.Ec2Client
+	DB        *postgres.DB
+	Tp        trace.Tracer
+}
