917b2af256489b8b5eb724afd232c383718ce43a
OsmanEmreZURNACI <emrezurnaci38@gmail.com>
2026-01-14 13:51:04 +0300

    feat(controllers): add EBS attach controller

---
+++ App/controllers/ec2/ebs/attach.go
@@ -0,0 +1,26 @@
+package ebs
+
+import (
+	"github.com/EmreZURNACI/InfrastructureAutomationControlPlane/app/aws/ec2/ebs"
+	"github.com/EmreZURNACI/InfrastructureAutomationControlPlane/controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func (srv *EbsService) Attach(c *fiber.Ctx) error {
+	var req ebs.AttachRequest
+
+	if err := c.BodyParser(&req); err != nil {
+		return c.JSON(controllers.FailureResponse(400, err.Error(), err.Error()))
+	}
+
+	if err := controllers.Validate(req); err != nil {
+		return c.JSON(controllers.FailureResponse(400, err.Error(), err.Error()))
+	}
+
+	_, err := srv.AttachService.Execute(c.UserContext(), &req)
+	if err != nil {
+		return c.JSON(controllers.FailureResponse(400, err.Error(), err.Error()))
+	}
+
+	return c.JSON(controllers.SuccessResponse(200, "the block device attached the instance successfully", nil))
+}
