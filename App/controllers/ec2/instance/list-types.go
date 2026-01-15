18a1738f378ce6ccf683cf83c3b625149050fbb9
OsmanEmreZURNACI <emrezurnaci38@gmail.com>
2026-01-15 16:42:35 +0300

    feat(controllers): add instance-types list controller

---
+++ App/controllers/ec2/instance/list-types.go
@@ -0,0 +1,17 @@
+package instance
+
+import (
+	"github.com/EmreZURNACI/InfrastructureAutomationControlPlane/app/aws/ec2/instance"
+	"github.com/EmreZURNACI/InfrastructureAutomationControlPlane/controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func (srv *InstanceService) ListTypes(c *fiber.Ctx) error {
+
+	res, err := srv.ListTypesService.Execute(c.UserContext(), &instance.ListInstanceTypeRequest{})
+	if err != nil {
+		return c.JSON(controllers.FailureResponse(400, err.Error(), err.Error()))
+	}
+
+	return c.JSON(controllers.SuccessResponse(200, *res.Message, res.InstanceType))
+}
