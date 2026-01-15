53b3591d233933d04da6ebe02c81c595c09cb672
OsmanEmreZURNACI <emrezurnaci38@gmail.com>
2026-01-15 16:46:51 +0300

    feat(controllers): add server response model

---
+++ App/controllers/response.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"github.com/EmreZURNACI/InfrastructureAutomationControlPlane/domain"
+)
+
+func SuccessResponse(code int, message string, data any) domain.Response {
+	model := domain.Response{
+		Status:     true,
+		StatusCode: code,
+		Message:    message,
+		Error:      nil,
+		Data:       data,
+	}
+
+	return model
+}
+func FailureResponse(code int, message string, err string) domain.Response {
+	model := domain.Response{
+		Status:     false,
+		StatusCode: code,
+		Message:    message,
+		Error:      &err,
+		Data:       nil,
+	}
+
+	return model
+}
