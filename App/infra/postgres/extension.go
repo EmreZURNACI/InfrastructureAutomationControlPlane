92ad890e26fedca99b2a5661979471fbbbe74e43
OsmanEmreZURNACI <emrezurnaci38@gmail.com>
2026-01-29 20:48:35 +0300

    feat(db): add UUID generation extension

---
+++ App/infra/postgres/extension.go
@@ -0,0 +1,13 @@
+package postgres
+
+import (
+	"github.com/EmreZURNACI/InfrastructureAutomationControlPlane/pkg/log"
+)
+
+func (h *DB) createExtension() error {
+	if err := h.db.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`).Error; err != nil {
+		log.Logger.Error("Extension kurulamadi")
+		return err
+	}
+	return nil
+}
