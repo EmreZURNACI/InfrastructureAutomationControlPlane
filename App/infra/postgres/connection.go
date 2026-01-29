9b5778121443885673fbd9b1f99fc951cc7037c0
OsmanEmreZURNACI <emrezurnaci38@gmail.com>
2026-01-29 20:47:24 +0300

    feat(db): add database connection

---
+++ App/infra/postgres/connection.go
@@ -0,0 +1,72 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/EmreZURNACI/InfrastructureAutomationControlPlane/pkg/config"
+	"github.com/EmreZURNACI/InfrastructureAutomationControlPlane/pkg/log"
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DB struct {
+	db *gorm.DB
+}
+
+func Connect() (*DB, error) {
+	var dsn string = fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		config.AppConfig.DatabaseConfig.Host,
+		config.AppConfig.DatabaseConfig.Port,
+		config.AppConfig.DatabaseConfig.Username,
+		config.AppConfig.DatabaseConfig.Password,
+		config.AppConfig.DatabaseConfig.Database,
+		config.AppConfig.DatabaseConfig.SSLMode,
+	)
+
+	con, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Logger.Error("database connection failed")
+		return nil, err
+	}
+
+	var status bool = false
+	for key := range 3 {
+		if err := con.Ping(); err != nil {
+			log.Logger.Info(fmt.Sprintf("%d. deneme başarısız", key+1))
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		status = true
+		break
+	}
+
+	if !status {
+		log.Logger.Error("database connection failed")
+		return nil, err
+	}
+
+	Db, err := gorm.Open(
+		postgres.New(postgres.Config{
+			Conn: con,
+		}), &gorm.Config{
+			// NamingStrategy: schema.NamingStrategy{
+			// 	TablePrefix:   config.AppConfig.DatabaseConfig.Schema,
+			// 	SingularTable: false,
+			// },
+		},
+	)
+
+	if err != nil {
+		log.Logger.Error("database connection failed")
+		return nil, err
+	}
+
+	return &DB{
+		db: Db,
+	}, nil
+}
